f7fbae9aaced174133005152bb8586a4257df59a
Kylie Benfield <admin@shadrian.com>
2018-04-13 21:24:52 -0500

    Initial Commit

---
+++ Numismatics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Numismatics
+//
+//  Created by Kylie Benfield on 4/13/18.
+//  Copyright © 2018 Kylie Benfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
